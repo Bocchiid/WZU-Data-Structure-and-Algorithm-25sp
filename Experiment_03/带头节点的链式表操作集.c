6bc671b623dc8da5cbb0086c393fcc32e77f0f9e
Bocchiid <487883857@qq.com>
2025-03-15 12:46:23 +0800

    Add 带头节点的链式表操作集.c

---
+++ Experiment_03/带头节点的链式表操作集.c
@@ -0,0 +1,86 @@
+List MakeEmpty()
+{
+    List L = (List)malloc(sizeof(struct LNode) * 1);
+    L->Next = NULL;
+
+    return L;
+}
+Position Find( List L, ElementType X )
+{
+    Position p;
+
+    p = L->Next;
+
+    while (p)
+    {
+        if (p->Data == X)
+            return p;
+
+        p = p->Next;
+    }
+
+    return ERROR;
+}
+bool Insert( List L, ElementType X, Position P )
+{
+    Position p, q;
+    Position s;
+
+    q = L;
+    p = L->Next; // p = q->Next;
+
+    while (p)
+    {
+        if (p == P)
+            break;
+
+        q = p;
+        p = p->Next;
+    }
+
+    if (p == P)
+    {
+        s = (Position)malloc(sizeof(struct LNode) * 1);
+        s->Data = X;
+        s->Next = q->Next;
+        q->Next = s;
+
+        return true;
+    }
+    else
+    {
+        printf("Wrong Position for Insertion\n");
+
+        return false;
+    }
+}
+bool Delete( List L, Position P )
+{
+    Position p, q;
+
+    q = L;
+    p = L->Next; // p = q->Next;
+
+    while (p)
+    {
+        if (p == P)
+            break;
+
+        q = p;
+        p = p->Next;
+    }
+
+    if (p)
+    {
+        q->Next = p->Next;
+        free(p); // 这句话可以不要
+
+        return true;
+    }
+    else
+    {
+        printf("Wrong Position for Deletion\n");
+
+        return false;
+    }
+}
