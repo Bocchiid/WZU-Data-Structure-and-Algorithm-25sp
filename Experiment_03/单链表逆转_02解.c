78dd2cdf929928197b3a9a9c5dfdfcd76b3ee48d
Bocchiid <487883857@qq.com>
2025-03-16 20:08:42 +0800

    Add 单链表逆转_02解.c

--- Experiment_03/单链表逆转_02解.c
+++ Experiment_03/单链表逆转_02解.c
@@ -1,14 +1,16 @@
+/* 该解法为利用头插法逆置链表的解法 */
+
 List Reverse( List L )
 {
     List dummy = (List)malloc(sizeof(struct Node) * 1);
-    dummy->Next = NULL; // dummy->Next = nullptr;
+    dummy->Next = NULL;
     List p;
     List s;
 
     p = L;
-    
+
     while (p)
-    {    // 头插法逆置链表
+    {
         s = (List)malloc(sizeof(struct Node) * 1);
         s->Data = p->Data;
         s->Next = dummy->Next;
@@ -16,8 +18,8 @@ List Reverse( List L )
         p = p->Next;
     }
 
-    if (L) // 由输出样例可以知道, L1逆置后应该只有一个结点, 前提是L1不为空
-        L->Next = NULL; // L->Next = nullptr;
+    if (L)
+        L->Next = NULL; // 由输出样例可以知道, L1逆置后应该只有一个结点, 前提是L1不为空
 
     return dummy->Next;
 }
