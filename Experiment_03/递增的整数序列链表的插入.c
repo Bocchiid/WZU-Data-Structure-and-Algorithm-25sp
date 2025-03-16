32facec04d3001b17b735b8681a171538347e28d
Bocchiid <487883857@qq.com>
2025-03-16 22:15:05 +0800

    Add 递增的整数序列链表的插入.c

--- Experiment_03/递增的整数序列链表的插入.c
+++ Experiment_03/递增的整数序列链表的插入.c
@@ -1,9 +1,13 @@
+/* 注意题目描述, 该链表为带头节点链表 */
+/* 本题实现的其实是链表的插入排序, 即遍历寻找插入位置 + 结点的插入两个操作 */
+
 List Insert( List L, ElementType X )
 {
+    List s;
     List p, q;
 
     q = L;
-    p = L->Next;
+    p = L->Next; // p = q->Next;
 
     while (p)
     {
@@ -14,7 +18,7 @@ List Insert( List L, ElementType X )
         p = p->Next;
     }
 
-    List s = (List)malloc(sizeof(struct Node) * 1);
+    s = (List)malloc(sizeof(struct Node) * 1);
     s->Data = X;
     s->Next = q->Next;
     q->Next = s;
