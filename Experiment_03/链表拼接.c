29fca5076708cc8e0c10a96e82eaeb311894eff4
Bocchiid <487883857@qq.com>
2025-03-16 21:49:22 +0800

    Add 链表拼接.c

--- Experiment_03/链表拼接.c
+++ Experiment_03/链表拼接.c
@@ -1,32 +1,36 @@
+/* 该解法为带头节点链表的解法, 推荐使用带头节点链表, 不然有你好受的 */
+/* typedef用一下, 不然代码太长了, 写起来有你好受的 */
+
 struct ListNode *mergelists(struct ListNode *list1, struct ListNode *list2)
 {
-    struct ListNode *dummy = (struct ListNode*)malloc(sizeof(struct ListNode) * 1);
-    dummy->next = NULL; // dummy->next = nullptr;
-    struct ListNode *s;
-    struct ListNode *tail = dummy;
+    typedef struct ListNode node;
+    node *dummy = (node*)malloc(sizeof(node) * 1);
+    node *s;
+    node *tail = dummy;
 
     while (list1 && list2)
     {
-        s = (struct ListNode*)malloc(sizeof(struct ListNode) * 1);
+        s = (node*)malloc(sizeof(node) * 1);
 
         if (list1->data < list2->data)
         {
             s->data = list1->data;
+            tail->next = s;
+            tail = s;
             list1 = list1->next;
         }
         else
         {
             s->data = list2->data;
+            tail->next = s;
+            tail = s;
             list2 = list2->next;
         }
-
-        tail->next = s;
-        tail = s;
     }
 
     while (list1)
     {
-        s = (struct ListNode*)malloc(sizeof(struct ListNode) * 1);
+        s = (node*)malloc(sizeof(node) * 1);
         s->data = list1->data;
         tail->next = s;
         tail = s;
@@ -35,7 +39,7 @@ struct ListNode *mergelists(struct ListNode *list1, struct ListNode *list2)
 
     while (list2)
     {
-        s = (struct ListNode*)malloc(sizeof(struct ListNode) * 1);
+        s = (node*)malloc(sizeof(node) * 1);
         s->data = list2->data;
         tail->next = s;
         tail = s;
