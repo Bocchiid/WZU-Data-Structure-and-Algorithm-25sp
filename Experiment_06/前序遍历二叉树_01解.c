0ee49c170ccf7514f55ebcce34ea5ed1b74b67f5
Bocchiid <487883857@qq.com>
2025-03-29 23:12:11 +0800

    Add 前序遍历二叉树_01解.c

---
+++ Experiment_06/前序遍历二叉树_01解.c
@@ -0,0 +1,46 @@
+/* 本题就是在构建二叉树的基础上, 增加了前序遍历 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct tnode
+{
+    int val;
+    struct tnode *left;
+    struct tnode *right;
+} tnode;
+
+tnode *createTnode(int val, tnode *left, tnode *right)
+{
+    tnode *s = (tnode *)malloc(sizeof(tnode) * 1);
+    s->val = val;
+    s->left = left;
+    s->right = right;
+
+    return s;
+}
+
+void preorder(tnode *root)
+{
+    if (root)
+    {
+        printf("%d\n", root->val);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+int main()
+{
+    int val;
+
+    scanf("%d", &val);
+    tnode *left = createTnode(val, NULL, NULL);
+    scanf("%d", &val);
+    tnode *right = createTnode(val, NULL, NULL);
+    scanf("%d", &val);
+    tnode *root = createTnode(val, left, right);
+    preorder(root);
+
+    return 0;
+}
