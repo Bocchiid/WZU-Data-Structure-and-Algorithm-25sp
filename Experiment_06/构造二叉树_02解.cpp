be19d938d2dae5154a910dc11d10887e391b1c2c
Bocchiid <487883857@qq.com>
2025-03-29 21:56:29 +0800

    Add 构造二叉树_02解.cpp

---
+++ Experiment_06/构造二叉树_02解.cpp
@@ -0,0 +1,17 @@
+/* 此解法虽然能过OJ, 但完全不符合题目要求, 对树的学习不利 */
+/* 所以, 还请读者自觉学习树的做法 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int left, right, root;
+
+    cin >> left >> right >> root;
+    cout << "root data = " << root << endl
+         << "left child data = " << left << endl
+         << "right child data = " << right << endl;
+
+    return 0;
+}
