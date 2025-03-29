d9618e0163fbcb25d5cbc80e9c84de785c09f0e6
Bocchiid <487883857@qq.com>
2025-03-29 23:45:33 +0800

    Add 层序遍历二叉树_02解.c

---
+++ Experiment_06/层序遍历二叉树_02解.c
@@ -0,0 +1,16 @@
+/* 此解法虽然能过OJ, 但完全不符合题目要求, 对树的学习不利 */
+/* 所以, 还请读者自觉学习树的做法 */
+
+#include <stdio.h>
+
+int main()
+{
+    int left, right, root;
+
+    scanf("%d %d %d", &left, &right, &root);
+    printf("%d\n", root);
+    printf("%d\n", left);
+    printf("%d\n", right);
+
+    return 0;
+}
