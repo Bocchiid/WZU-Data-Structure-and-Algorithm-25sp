d7b343bb3011e9851e5c274288d4301b95d11529
Bocchiid <487883857@qq.com>
2025-03-29 23:37:34 +0800

    Add 后序遍历二叉树_02解.cpp

---
+++ Experiment_06/后序遍历二叉树_02解.cpp
@@ -0,0 +1,17 @@
+/* 此解法虽然能过OJ, 但完全不符合题目要求, 对树的学习不利 */
+/* 所以, 还请读者自觉学习树的做法 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int left, right, root;
+
+    cin >> left >> right >> root;
+    cout << left << endl
+         << right << endl
+         << root << endl;
+
+    return 0;
+}
