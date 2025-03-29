919b3b9b62adb6bf5f100a9349e7452977c14498
Bocchiid <487883857@qq.com>
2025-03-29 22:20:14 +0800

    Add 前序遍历二叉树_01解.cpp

---
+++ Experiment_06/前序遍历二叉树_01解.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct tnode
+{
+    int val;
+    tnode *left;
+    tnode *right;
+
+    tnode(int val) : val(val), left(left), right(right) {}
+    tnode(int val, tnode *left, tnode *right) : val(val), left(left), right(right) {}
+};
+
+void preorder(tnode *root)
+{
+    if (root)
+    {
+        cout << root->val << endl;
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+int main()
+{
+    int val;
+
+    cin >> val;
+    tnode *left = new tnode(val);
+    cin >> val;
+    tnode *right = new tnode(val);
+    cin >> val;
+    tnode *root = new tnode(val, left, right);
+    preorder(root);
+
+    return 0;
+}
