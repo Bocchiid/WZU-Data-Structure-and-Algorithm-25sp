326528ff0e34aa83f4b638cf2f9287d7768b65f4
Bocchiid <487883857@qq.com>
2025-03-31 22:55:41 +0800

    Add 将表达式树转换成中缀表达式_通解.cpp

---
+++ Experiment_06/将表达式树转换成中缀表达式_通解.cpp
@@ -0,0 +1,53 @@
+/* 注此通解不能过OJ */
+
+/* 本题就是在构建二叉树的基础上, 增加了中序遍历 + 在特定时间点执行特定操作 */
+/* 二叉树的一种理解就是在前中后三个特定的时间点, 执行操作*/
+
+#include <iostream>
+using namespace std;
+
+struct tnode
+{
+    string val;
+    tnode *left;
+    tnode *right;
+
+    tnode(string val) : val(val), left(nullptr), right(nullptr) {}
+    tnode(string val, tnode *left, tnode *right) : val(val), left(left), right(right) {}
+};
+
+void inorder(tnode *root, int deep)
+{
+    if (root)
+    {
+        if (!root->left && !root->right)
+            cout << root->val;
+        else
+        {
+            if (deep > 1)
+                cout << "(";
+
+            inorder(root->left, deep + 1);
+            cout << root->val;
+            inorder(root->right, deep + 1);
+
+            if (deep > 1)
+                cout << ")";
+        }
+    }
+}
+
+int main()
+{
+    string val;
+
+    cin >> val;
+    tnode *left = new tnode(val);
+    cin >> val;
+    tnode *right = new tnode(val);
+    cin >> val;
+    tnode *root = new tnode(val, left, right);
+    inorder(root, 1);
+
+    return 0;
+}
