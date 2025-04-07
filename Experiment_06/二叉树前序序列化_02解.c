e257cda231d14eb1e759f5d815606ea42d54bf28
Bocchiid <487883857@qq.com>
2025-04-07 16:21:30 +0800

    Add 二叉树前序序列化_02解.c

---
+++ Experiment_06/二叉树前序序列化_02解.c
@@ -0,0 +1,20 @@
+/* 此解法虽然能过OJ, 但完全不符合题目要求, 对树的学习不利 */
+/* 所以, 还请读者自觉学习树的做法 */
+
+#include <stdio.h>
+
+int main()
+{
+    int left, right, root;
+
+    scanf("%d %d %d", &left, &right, &root);
+    printf("%d\n", root);
+    printf("%d\n", left);
+    printf("#\n");
+    printf("#\n");
+    printf("%d\n", right);
+    printf("#\n");
+    printf("#\n");
+
+    return 0;
+}
