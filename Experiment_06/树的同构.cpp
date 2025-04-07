17f12a646aaf825d60b107dc85ceeb228e2fdf46
Bocchiid <487883857@qq.com>
2025-04-07 21:34:48 +0800

    Add 树的同构.cpp

--- Experiment_06/树的同构.cpp
+++ Experiment_06/树的同构.cpp
@@ -1,19 +1,24 @@
-/**/
-
-/* 此解法虽然能过OJ, 但完全不符合题目要求, 对树的学习不利 */
-/* 所以, 还请读者自觉学习树的做法 */
+/* 该解法中, 构建二叉树和寻找二叉树根的位置和 题目: 列出叶节点 一致 */
+/* 本题重点在于如何判断两棵树是否同构 */
+/* 注重对同构情况的分类 */
+/* 1. 二树皆空, 同构 */
+/* 2. 一树为空, 不同构 */
+/* 3. 两树根得相同才能同构 */
+/* 4. 两树相同, 同构 */
+/* 5. 两树交换后相同, 同构 */
+/* 6. else 不同构 */
 
 #include <iostream>
 #include <vector>
 using namespace std;
-
+/* 二叉树结点的定义 */
 struct tnode
 {
     char val;
     int left;
     int right;
 };
-
+/* 判断是否同构 */
 bool isomorphic(int root1, int root2, vector<tnode> a, vector<tnode> b)
 { /* 1. 两树都为空树 */
     if (root1 == -1 && root2 == -1)
@@ -49,7 +54,7 @@ int main()
     cin >> n;
     vector<tnode> a(n);
     vector<int> check1(n);
-
+    /* 构建二叉树1 */
     for (i = 0; i < n; i++)
     {
         cin >> val >> left >> right;
@@ -74,7 +79,7 @@ int main()
     }
 
     cin >> m;
-
+    /* 若结点数量不同, 则必不同构 */
     if (n != m)
     {
         cout << "No" << endl;
@@ -83,7 +88,7 @@ int main()
 
     vector<tnode> b(m);
     vector<int> check2(m);
-
+    /* 构建二叉树2 */
     for (i = 0; i < m; i++)
     {
         cin >> val >> left >> right;
@@ -109,21 +114,21 @@ int main()
 
     int root1 = -1;
     int root2 = -1;
-
+    /* 寻找二叉树1的根的位置 */
     for (i = 0; i < n; i++)
         if (!check1[i])
         {
             root1 = i;
             break;
         }
-
+    /* 寻找二叉树2的根的位置 */
     for (i = 0; i < m; i++)
         if (!check2[i])
         {
             root2 = i;
             break;
         }
-
+    /* 判断是否同构 */
     if (isomorphic(root1, root2, a, b))
         cout << "Yes" << endl;
     else
