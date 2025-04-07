db6e1f5f0135e02dc86a2ad0b80b1f2c0c662519
Bocchiid <487883857@qq.com>
2025-04-07 16:44:14 +0800

    Add 二叉树层序序列化_02解.cpp

---
+++ Experiment_06/二叉树层序序列化_02解.cpp
@@ -0,0 +1,21 @@
+/* 此解法虽然能过OJ, 但完全不符合题目要求, 对树的学习不利 */
+/* 所以, 还请读者自觉学习树的做法 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int left, right, root;
+
+    cin >> left >> right >> root;
+    cout << root << endl
+         << left << endl
+         << right << endl
+         << "#" << endl
+         << "#" << endl
+         << "#" << endl
+         << "#" << endl;
+
+    return 0;
+}
