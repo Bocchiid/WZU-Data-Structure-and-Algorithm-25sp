2eb3d6ede0b5e98a1a84525009f362c5aa92c7e9
Bocchiid <487883857@qq.com>
2025-04-22 23:35:38 +0800

    Add 二叉堆的 4 种操作_01解.cpp

---
+++ Experiment_08/二叉堆的 4 种操作_01解.cpp
@@ -0,0 +1,86 @@
+/* 由于二叉堆不唯一 */
+/* 本题请严格按照percolateDown(下滤/下沉)调整建立二叉堆 */
+/* 01和02解的区别在于是否将数组作为函数形参 */
+/* 01解的实现是将数组作为形参处理 */
+
+/* 注: percolateUp为函数6-1的02解实现 */
+/* 注: percolateDown为函数6-1的02解实现 */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void percolateUp(vector<int> &a, int k)
+{
+    while (k > 1)
+    {
+        if (a[k / 2] > a[k])
+        {
+            swap(a[k / 2], a[k]);
+            k /= 2;
+        }
+        else
+            break;
+    }
+}
+
+void percolateDown(vector<int> &a, int k)
+{
+    int size = a.size() - 1;
+
+    for (int i = k * 2; i <= size; i *= 2)
+    {
+        if (i < size && a[i] > a[i + 1])
+            i++;
+
+        if (a[i] < a[k])
+        {
+            swap(a[k], a[i]);
+            k = i;
+        }
+        else
+            break;
+    }
+}
+
+void Delete(vector<int> &a)
+{
+    vector<int> temp = a;
+    a.resize(a.size() - 1);
+
+    for (int i = 1; i < a.size(); i++)
+        a[i] = temp[i];
+
+    a[1] = temp[a.size()];
+
+    for (int i = (a.size() - 1) / 2; i >= 1; i--)
+        percolateDown(a, i);
+}
+
+int main()
+{
+    int i;
+    int c;
+    int n;
+    int x;
+
+    cin >> c;
+    cin >> n;
+    vector<int> a(n + 2);
+
+    for (i = 1; i <= n; i++)
+        cin >> a[i];
+
+    cin >> x;
+    n++;
+    a[n] = x;
+    percolateUp(a, n);
+
+    cout << "min = " << a[1] << endl;
+    Delete(a);
+
+    for (i = 1; i < a.size(); i++)
+        cout << a[i] << endl;
+
+    return 0;
+}
