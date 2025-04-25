5099768ec1fad8e2aea6520ba33b36caad03130e
Bocchiid <487883857@qq.com>
2025-04-25 19:34:10 +0800

    Add 二叉堆的快速建堆操作_02解.cpp

--- Experiment_08/二叉堆的快速建堆操作_02解.cpp
+++ Experiment_08/二叉堆的快速建堆操作_02解.cpp
@@ -3,27 +3,25 @@
 /* 01和02解的区别在于是否将数组作为函数形参 */
 /* 02解的实现是将数组作为全局变量 */
 
-/* 注: percolateDown为函数6-1的02解实现 */
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
 vector<int> a;
-
-void percolateDown(int k)
+/* percolateDown()为6-1的02解实现 */
+void percolateDown(int p)
 {
     int size = a.size() - 1;
-
-    for (int i = k * 2; i <= size; i *= 2)
+    
+    for (int i = p * 2; i <= size; i *= 2)
     {
         if (i < size && a[i] > a[i + 1])
             i++;
 
-        if (a[i] < a[k])
+        if (a[i] < a[p])
         {
-            swap(a[i], a[k]);
-            k = i;
+            swap(a[i], a[p]);
+            p = i;
         }
         else
             break;
@@ -32,7 +30,7 @@ void percolateDown(int k)
 
 int main()
 {
-    int i;
+    int i, j;
     int c;
     int n;
 
