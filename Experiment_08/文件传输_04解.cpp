33d1fb799a21e8186d0048ba77940b604887060c
Bocchiid <487883857@qq.com>
2025-04-26 23:55:23 +0800

    Add 文件传输_04解.cpp

---
+++ Experiment_08/文件传输_04解.cpp
@@ -0,0 +1,98 @@
+/* 本题考查并查集的find, union操作 */
+
+/* 03和04解和01和02解的区别在于数组是否从1开始计数 */
+/* 03和04解的区别在于是否将数组作为函数形参 */
+/* 04解的实现是将数组作为全局变量 */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> a;
+
+void init()
+{
+    for (int i = 0; i < a.size(); i++)
+        a[i] = -1;
+}
+
+int find(int p)
+{
+    if (a[p] < 0)
+        return p;
+
+    a[p] = find(a[p]); /* 路径压缩 */
+
+    return find(a[p]);
+}
+/* 按秩(rank)合并 */
+void unionByRank(int p, int q)
+{ /* 此处以树的元素个数为秩 */
+    int root1 = find(p);
+    int root2 = find(q);
+
+    if (root1 != root2)
+    {
+        if (a[root1] < a[root2])
+        {
+            a[root1] += a[root2];
+            a[root2] = root1;
+        }
+        else
+        {
+            a[root2] += a[root1];
+            a[root1] = root2;
+        }
+    }
+}
+
+bool isSameUnion(int p, int q)
+{
+    int root1 = find(p);
+    int root2 = find(q);
+
+    return root1 == root2;
+}
+
+int main()
+{
+    int i, j;
+    int n;
+
+    cin >> n;
+    a.resize(n);
+    init();
+
+    char op;
+    cin >> op;
+
+    int c1, c2;
+
+    while (op != 'S')
+    {
+        cin >> c1 >> c2;
+
+        if (op == 'I')
+            unionByRank(c1 - 1, c2 - 1);
+        else if (op == 'C')
+            if (isSameUnion(c1 - 1, c2 - 1))
+                cout << "yes" << endl;
+            else
+                cout << "no" << endl;
+        
+        cin >> op;
+    }
+
+    int cnt = 0;
+
+    for (i = 0; i < a.size(); i++)
+        if (a[i] < 0)
+            cnt++;
+
+    if (cnt == 1)
+        cout << "The network is connected." << endl;
+    else
+        cout << "There are " << cnt << " components." << endl;
+
+    return 0;
+}
