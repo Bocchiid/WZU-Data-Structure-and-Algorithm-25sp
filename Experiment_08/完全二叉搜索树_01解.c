fda74b95afe93d33bac12287e9e81f98e0a862ee
Bocchiid <487883857@qq.com>
2025-04-27 00:22:01 +0800

    Add 完全二叉搜索树_01解.c

--- Experiment_08/完全二叉搜索树_01解.c
+++ Experiment_08/完全二叉搜索树_01解.c
@@ -4,57 +4,44 @@
 /* 该解法是利用顺序表实现完全二叉树 */
 
 /* 01和02解的区别在于是否将数组作为函数形参 */
-/* 01解的实现是将数组作为形参处理 */
+/* 01解的实现是将数组作为函数形参 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int n;
 int cnt;
 int tree[1001];
 
-void swap(int *a, int *b)
+int cmp(void *a, void *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void bubbleSort(int a[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (a[j] > a[j + 1])
-                swap(&a[j], &a[j + 1]);
-        }
-    }
+    return (*(int *)a) > (*(int *)b);
 }
 
 void inorder(int a[], int root)
 {
-    if (root > n)
-        return;
+    if (root <= n)
+    {
+        inorder(a, root * 2);
 
-    inorder(a, root * 2);
+        tree[root] = a[cnt];
+        cnt++;
 
-    tree[root] = a[cnt];
-    cnt++;
-
-    inorder(a, root * 2 + 1);
+        inorder(a, root * 2 + 1);
+    }
 }
 
 int main()
 {
-    int i;
-    int a[1001] = {0};
+    int i, j;
 
     scanf("%d", &n);
+    int a[1001];
 
     for (i = 0; i < n; i++)
         scanf("%d", &a[i]);
 
-    bubbleSort(a, n);
+    qsort(a, n, sizeof(int), cmp);
 
     cnt = 0;
     inorder(a, 1);
