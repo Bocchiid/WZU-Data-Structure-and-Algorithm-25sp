f2cfa660cbd69493cccd9d9c9b5592f9f362155f
Bocchiid <487883857@qq.com>
2025-05-07 12:54:14 +0800

    Add 邻接表存储图的广度优先遍历_03解.c

---
+++ Experiment_09/邻接表存储图的广度优先遍历_03解.c
@@ -0,0 +1,41 @@
+void BFS ( LGraph Graph, Vertex S, void (*Visit)(Vertex) )
+{
+    if (Visited[S]) /** 若已经搜索过了, 就结束搜索 */
+        return;
+
+    Vertex q[1001];
+    int head = 0;
+    int tail = 0;
+
+    Vertex inq[1001] = {0};
+
+    q[tail] = S;
+    tail++;
+    inq[S] = 1;
+
+    while (head != tail)
+    {
+        Vertex t = q[head];
+        head++;
+        inq[t] = 0;
+
+        Visit(t);
+        Visited[t] = true;
+
+        PtrToAdjVNode p = Graph->G[t].FirstEdge;
+
+        while (p)
+        {
+            Vertex v = p->AdjV;
+
+            if (!Visited[v] && !inq[v])
+            {
+                q[tail] = v;
+                tail++;
+                inq[v] = 1;
+            }
+
+            p = p->Next;
+        }
+    }
+}
