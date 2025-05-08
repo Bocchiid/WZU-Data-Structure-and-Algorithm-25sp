bb313d63bbe11c1439e854d8de9aeff1f182a47d
Bocchiid <487883857@qq.com>
2025-05-08 14:57:26 +0800

    Add 广度优先遍历_03解.cpp

---
+++ Experiment_09/广度优先遍历_03解.cpp
@@ -0,0 +1,97 @@
+/** 本题重点在于bfs + 计算/输出连通分量 */
+/** 对于图的部分不做过多的注解 */
+
+/** 本题请严格使用邻接表存储图 */
+/** 该解法为邻接表的数组实现 */
+
+/** 本题较坑的点是要对边去重 */
+/** 该解法对去重, 使用的是map实现 */
+
+/** 奇数解为全局变量实现 */
+/** 偶数解为函数参数实现 */
+#define vc vector
+#define pb push_back
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+using namespace std;
+
+vc<char> key;
+vc<vc<int>> a;
+map<pair<int, int>, int> mp;
+vc<bool> visited;
+
+void bfs(int v)
+{
+    if (visited[v])
+        return;
+
+    queue<int> q;
+    q.push(v);
+
+    cout << key[v];
+    visited[v] = true;
+
+    while (!q.empty())
+    {
+        int t = q.front();
+        q.pop();
+
+        for (int i = a[t].size() - 1; i >= 0; i--)
+        {
+            int verx = a[t][i];
+
+            if (!visited[verx])
+            {
+                q.push(verx);
+
+                cout << key[verx];
+                visited[verx] = true;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int i, j;
+    int kMaxVertex;
+    int n, m;
+    int u, v, val;
+    /** input */
+    cin >> kMaxVertex;
+    cin >> n >> m;
+    /** input vertex */
+    key.resize(n);
+
+    for (i = 0; i < n; i++)
+        cin >> key[i];
+    /** input edge */
+    a.resize(n);
+
+    for (i = 0; i < m; i++)
+    {
+        cin >> u >> v >> val;
+
+        if (!mp.count({u, v}) && !mp.count({v, u}))
+        {
+            a[u].pb(v);
+            a[v].pb(u);
+            mp[{u, v}] = 1;
+            mp[{v, u}] = 1;
+        }
+    }
+    /** bfs */
+    visited.assign(n, false); // visited.resize(n, false);
+
+    for (i = 0; i < n; i++)
+        if (!visited[i])
+        {
+            bfs(i);
+            cout << endl;
+        }
+
+    return 0;
+}
