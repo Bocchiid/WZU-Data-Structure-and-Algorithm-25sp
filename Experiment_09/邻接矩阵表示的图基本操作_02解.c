69d7930b4041dd88822130530a2af1a405628ed0
Bocchiid <487883857@qq.com>
2025-05-09 23:53:48 +0800

    Add 邻接矩阵表示的图基本操作_02解.c

--- Experiment_09/邻接矩阵表示的图基本操作_02解.c
+++ Experiment_09/邻接矩阵表示的图基本操作_02解.c
@@ -32,7 +32,7 @@ void printVertex(char key[], int size)
 }
 /** 插入边 */
 void insertEdge(int a[][1001], int u, int v, int val)
-{
+{ /** 根据题目的意思, 本题不会出现重复输入的边, 但题目表述未提及，不严谨 */
     a[u][v] = val;
 }
 /** 删除边 */
