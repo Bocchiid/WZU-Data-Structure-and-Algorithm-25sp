3d1a910d184187963fffcff387bd3b875a10c79a
Bocchiid <487883857@qq.com>
2025-05-27 23:15:31 +0800

    Add 公路村村通_02解.c

--- Experiment_10/公路村村通_02解.c
+++ Experiment_10/公路村村通_02解.c
@@ -1,4 +1,5 @@
 /** 本题其实就是7-3prim算法的实现 */
+/** 由于题目不要求输出树的序列, 故不需要parent数组 */
 
 /** 该解法图的存储为邻接表实现 */
 /** 该解法的邻接表为链表实现 */
@@ -30,14 +31,10 @@ int n, m;
 
 edge *a[1001];
 int cost[1001];
-int parent[1001];
 
 int prim(int s)
 {
     int i, j;
-    /** Init parent */
-    for (i = 1; i <= n; i++)
-        parent[i] = 1;
     /** Init cost */
     for (i = 1; i <= n; i++)
         cost[i] = INF;
@@ -60,7 +57,6 @@ int prim(int s)
     /** Visit vertex s */
     weight += cost[s];
     cost[s] = 0;
-    parent[s] = -1;
 
     for (i = 1; i < n; i++)
     {
@@ -91,7 +87,6 @@ int prim(int s)
                 if (w < cost[v]) /** cost[u] + w < cost[v] */
                 {
                     cost[v] = w; /** cost[v] = cost[u] + w */
-                    parent[v] = u;
                 }
 
                 p = p->next;
