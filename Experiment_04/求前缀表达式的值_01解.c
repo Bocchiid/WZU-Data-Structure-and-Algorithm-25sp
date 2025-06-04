2fec67d86ad1b583e7c65e298acb869488eeed65
Bocchiid <487883857@qq.com>
2025-06-04 16:23:05 +0800

    Add 求前缀表达式的值_01解.c

---
+++ Experiment_04/求前缀表达式的值_01解.c
@@ -0,0 +1,137 @@
+/** 本题的坑在于: 输入可能存在负数和小数 */
+
+/** 01和02解的区别在于是否是函数式实现 */
+/** 01解为非函数式实现 */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j;
+    char s[101];
+
+    gets(s);
+    /** Make a stack */
+    double stk[101];
+    int top = -1;
+
+    double a, b;
+
+    for (i = strlen(s) - 1; i >= 0; i--)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            char s1[101];
+            int length = 101;
+
+            length--;
+            s1[length] = '\0';
+
+            for (j = i; j >= 0 && (s[j] >= '0' && s[j] <= '9' || s[j] == '.'); j--)
+            {
+                length--;
+                s1[length] = s[j];
+            }
+
+            if (j >= 0 && s[j] == '-')
+            {
+                length--;
+                s1[length] = s[j];
+            }
+
+            char *p = &s1[length];
+
+            top++;
+            stk[top] = atof(p);
+
+            i = j;
+        }
+        else if (s[i] == '+')
+        {
+            if (top < 1)
+            {
+                printf("ERROR\n");
+
+                return 0;
+            }
+
+            a = stk[top];
+            top--;
+            b = stk[top];
+            top--;
+
+            top++;
+            stk[top] = a + b;
+        }
+        else if (s[i] == '-')
+        {
+            if (top < 1)
+            {
+                printf("ERROR\n");
+
+                return 0;
+            }
+
+            a = stk[top];
+            top--;
+            b = stk[top];
+            top--;
+
+            top++;
+            stk[top] = a - b;
+        }
+        else if (s[i] == '*')
+        {
+            if (top < 1)
+            {
+                printf("ERROR\n");
+
+                return 0;
+            }
+
+            a = stk[top];
+            top--;
+            b = stk[top];
+            top--;
+
+            top++;
+            stk[top] = a * b;
+        }
+        else if (s[i] == '/')
+        {
+            if (top < 1)
+            {
+                printf("ERROR\n");
+
+                return 0;
+            }
+
+            a = stk[top];
+            top--;
+            b = stk[top];
+            top--;
+
+            if (b == 0)
+            {
+                printf("ERROR\n");
+
+                return 0;
+            }
+
+            top++;
+            stk[top] = a / b;
+        }
+    }
+
+    if (top == 0)
+    {
+        printf("%.1f\n", stk[top]);
+        top--;
+    }
+    else
+        printf("ERROR\n");
+
+    return 0;
+}
