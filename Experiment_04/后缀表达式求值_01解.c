135e4e91bf43b4953dd8a8f4c255586865e68bb0
Bocchiid <487883857@qq.com>
2025-06-04 15:18:40 +0800

    Add 后缀表达式求值_01解.c

---
+++ Experiment_04/后缀表达式求值_01解.c
@@ -0,0 +1,149 @@
+/** 本题没有什么坑, 请放心食用 */
+
+/** 01和02解的区别在于是否是函数式实现 */
+/** 01解为非函数式实现 */
+
+#define ERROR 1000000000
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j;
+    char s[1001];
+    /** Make a stack */
+    int stk[1001];
+    int top = -1;
+
+    int a, b;
+
+    while (scanf("%s", s) != EOF)
+    {
+        if (strcmp(s, "+") == 0)
+        {
+            if (top < 1)
+            {
+                printf("错误：表达式不规范。\n");
+                printf("%d\n", ERROR);
+
+                return 0;
+            }
+
+            b = stk[top];
+            top--;
+            a = stk[top];
+            top--;
+
+            top++;
+            stk[top] = a + b;
+        }
+        else if (strcmp(s, "-") == 0)
+        {
+            if (top < 1)
+            {
+                printf("错误：表达式不规范。\n");
+                printf("%d\n", ERROR);
+
+                return 0;
+            }
+
+            b = stk[top];
+            top--;
+            a = stk[top];
+            top--;
+
+            top++;
+            stk[top] = a - b;
+        }
+        else if (strcmp(s, "*") == 0)
+        {
+            if (top < 1)
+            {
+                printf("错误：表达式不规范。\n");
+                printf("%d\n", ERROR);
+
+                return 0;
+            }
+
+            b = stk[top];
+            top--;
+            a = stk[top];
+            top--;
+
+            top++;
+            stk[top] = a * b;
+        }
+        else if (strcmp(s, "/") == 0)
+        {
+            if (top < 1)
+            {
+                printf("错误：表达式不规范。\n");
+                printf("%d\n", ERROR);
+
+                return 0;
+            }
+
+            b = stk[top];
+            top--;
+            a = stk[top];
+            top--;
+
+            if (b == 0)
+            {
+                printf("错误：除法操作分母为零。\n");
+                printf("%d\n", ERROR);
+
+                return 0;
+            }
+
+            top++;
+            stk[top] = a / b;
+        }
+        else if (strcmp(s, "%") == 0)
+        {
+            if (top < 1)
+            {
+                printf("错误：表达式不规范。\n");
+                printf("%d\n", ERROR);
+
+                return 0;
+            }
+
+            b = stk[top];
+            top--;
+            a = stk[top];
+            top--;
+
+            if (b == 0)
+            {
+                printf("错误：取模操作除数为零。\n");
+                printf("%d\n", ERROR);
+
+                return 0;
+            }
+
+            top++;
+            stk[top] = a % b;
+        }
+        else
+        { /** Transform string into int and push it to stack */
+            top++;
+            stk[top] = atoi(s);
+        }
+    }
+
+    if (top == 0)
+    {
+        printf("%d\n", stk[top]);
+        top--;
+    }
+    else
+    {
+        printf("错误：表达式不规范。\n");
+        printf("%d\n", ERROR);
+    }
+
+    return 0;
+}
